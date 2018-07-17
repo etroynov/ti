397e7804b01bbb99d3dc3db216331a37e176d09f
troynov.e <troynov.e@diamirerp.com>
2018-07-17 19:14:02 +0300

    #master. fix next.js configs

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -1,2 +1,2 @@
-const withAwesomeTypescript = require("next-awesome-typescript");
-module.exports = withAwesomeTypescript();
+const withTypescript = require('@zeit/next-typescript')
+module.exports = withTypescript()
