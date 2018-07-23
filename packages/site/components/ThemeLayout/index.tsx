56b50f1dad6b026362f9cb843320cd69c8a720b2
Evgeniy Troynov <etroynov@gmail.com>
2018-07-23 09:29:55 +0500

    #master. code refactor, add ThemeLayout

---
+++ packages/site/components/ThemeLayout/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * Dependencies
+ */
+
+import * as React from 'react';
+
+/**
+ * Expo
+ */
+
+const ThemeLayout = (props) => (
+  <div>{props.children}</div>
+);
+
+export default ThemeLayout;
