c9b5e5be48161faf828b2c90fa06b278354f9e0d
nanbratan <morfemi9498@gmail.com>
2018-07-29 19:32:51 +0300

    TI-5. MatchHistory component - divided into small components, refactor code, add styled components

---
+++ packages/site/components/MatchHistContent/index.tsx
@@ -0,0 +1,50 @@
+/*!
+ * dependencies
+ */
+import * as React from 'react';
+
+/*!
+ * Styled Component
+ */
+
+import {
+  TeamAvatar,
+  TeamTitle,
+  TemaInfo,
+  Center,
+  Right,
+  Left,
+  Score,
+  Date,
+  Meeting,
+  Content,
+} from './style';
+
+export default class App extends React.Component {
+  render() {
+    const { date, firstTeam, firstTeamAvatar, score, secondTeam, secondTeamAvatar, meetName, meetStage } = this.props;
+    return (
+      <Content>
+        <Date>{date}</Date>
+        <Score>
+          <Left>
+            <TemaInfo>
+              <TeamTitle>{firstTeam}</TeamTitle>
+            </TemaInfo>
+            <TeamAvatar src={firstTeamAvatar} />
+          </Left>
+          <Center>{score}</Center>
+          <Right>
+            <TeamAvatar src={secondTeamAvatar} />
+            <TemaInfo>
+              <TeamTitle>{secondTeam}</TeamTitle>
+            </TemaInfo>
+          </Right>
+        </Score>
+        <Meeting>
+          <span>{meetName}</span> | <span>{meetStage}</span>
+        </Meeting>
+      </Content>
+    )
+  }
+}
